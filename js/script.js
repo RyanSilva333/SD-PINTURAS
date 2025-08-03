d1b20e3c041bb3b1bcfa7d3ac7425114d5a46d5f
RyanSilva333 <ryanvieira.vieira@gmail.com>
2025-08-02 22:40:15 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 const galeria = document.getElementById("galeria");
-
-// Supondo que você tenha 100 imagens nomeadas como foto1.jpg, foto2.jpg...
 const totalImagens = 27;
 
 for (let i = 1; i <= totalImagens; i++) {
